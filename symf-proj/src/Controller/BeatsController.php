07668ae9e250a693a405de31fc0f9cabb7835576
vlad <stimulmonk@yandex.ru>
2020-02-08 13:09:47 +0300

    monolog logging

--- symf-proj/src/Controller/BeatsController.php
+++ symf-proj/src/Controller/BeatsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Beat;
 use App\Repository\BeatRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
