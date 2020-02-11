2046412bf9fe9f076be5c1d11e9b6b664ddfc7e4
Dmitry Zakovyrin <dmitryzakovyrin@gmail.com>
2020-02-12 01:20:43 +0300

    travis ci

--- build.sh
+++ build.sh
@@ -3,9 +3,4 @@
 cd ./symf-proj
 composer install
 php bin/phpunit
-cd bin
-ls -lah
-cd .phpunit
-ls -lah
-cd ../..
-vendor/bin/phpstan analyse src tests --level 5
+vendor/bin/phpstan analyse src --level 5
