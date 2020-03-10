6603354dcbd9a52fe562fc59a8f5bdf132cafa25
Dmitry Zakovyrin <dmitryzakovyrin@gmail.com>
2020-03-10 23:01:26 +0300

    up project

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 docker-compose up -d --build
 cd symf-proj || exit
-composer install
+composer update
 yarn install
+yarn encore dev
 
 #create structure
 cd public &&
@@ -11,21 +12,24 @@ cd public &&
   mkdir "beats" &&
   mkdir "images" && mkdir "images/beats" &&
   mkdir "samples"
+  cd ../..
+
+export $(cat .env | xargs)
 
 #rights and owner of directories and files
 echo 'Меняем пользователя'
-docker exec test-symf_php_1 bash -c 'chown -R www-data:www-data .'
+docker exec ${CONTAINER_PREFIX}_php bash -c 'chown -R www-data:www-data .'
 
 echo 'Перераздаем права'
-docker exec test-symf_php_1 bash -c 'find ./public -type f -exec chmod 644 {} \;'
-docker exec test-symf_php_1 bash -c 'find ./public -type d -exec chmod 755 {} \;'
+docker exec ${CONTAINER_PREFIX}_php bash -c 'find ./public -type f -exec chmod 644 {} \;'
+docker exec ${CONTAINER_PREFIX}_php bash -c 'find ./public -type d -exec chmod 755 {} \;'
 
 #update database
 echo 'Обновляем БД'
-docker exec test-symf_php_1 bash -c 'php bin/console doctrine:schema:update --force'
+docker exec ${CONTAINER_PREFIX}_php bash -c 'php bin/console doctrine:schema:update --force'
 
 #generate categories and genres
 echo 'Парсим и сохраняем жанры с категориями'
-docker exec test-symf_php_1 bash -c 'php bin/console parse:genres ; php bin/console parse:categories'
+docker exec ${CONTAINER_PREFIX}_php bash -c 'php bin/console parse:genres ; php bin/console parse:categories'
 
 echo 'Готово!'
